6647536f829dcce9ccb9b03b61bec0b65461ab03
Liam Mueller <l.mueller@fetchrewards.com>
2023-01-10 14:35:42 -0600

    Moving to nvim-lspconfig and cmp.

--- lua/mule/packer.lua
+++ lua/mule/packer.lua
@@ -1,4 +1,4 @@
-vim.cmd('packadd packer.nvim')
+	vim.cmd('packadd packer.nvim')
 
 local __print = print
 local debug = false
@@ -19,46 +19,29 @@ local plugins = {
 	'wbthomason/packer.nvim', -- Packer can manage itself
 	'preservim/nerdtree',
 	'tpope/vim-fugitive',
+	'ray-x/go.nvim',
+	'ray-x/guihua.lua',
+	'neovim/nvim-lspconfig',
+	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-nvim-lsp',
 	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	},
 	{
 		'nvim-treesitter/nvim-treesitter',
 		run = function()
-			require('nvim-treesitter.install').
-				update({ with_sync = true })()
+			require('nvim-treesitter.install').update({ with_sync = true })()
 		end
 	},
 	{
-	    'rose-pine/neovim',
-	    as = 'rose-pine',
-	    config = function()
-		vim.cmd('colorscheme rose-pine')
-	    end
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
 	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
 }
 
 local function print(s)
