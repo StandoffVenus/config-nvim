03884dc6669e49358d0c758a4129ec8312c66da5
Liam Mueller <l.mueller@fetchrewards.com>
2025-01-22 14:36:48 -0600

    fix: load plugins before notifying

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ local DEBUG = vim.log.levels.DEBUG
 local ERROR = vim.log.levels.ERROR
 
 local modules = {
-	'opts',
-	'remaps',
+	'remaps', -- Remaps have to happen before Lazy is loaded
 	'lazy',
+	'path',
 	'filetypes',
 	'lsp_extensions',
-	'path',
+	'opts',
 }
 
 local config_dir = vim.fn.expand('~')
@@ -20,12 +20,18 @@ if not config_dir then
 end
 
 local mod_base = 'mule'
+local loaded_mods = {}
 for _, mod_name in pairs(modules) do
 	local mod = mod_base .. '.' .. mod_name
 	local ok, err = pcall(require, mod)
 	if not ok then
 		vim.notify('failed to load module "' .. mod .. '": ' .. tostring(err), ERROR)
 	else
-		vim.notify('loaded module "' .. mod .. '"', DEBUG)
+		table.insert(loaded_mods, mod)
 	end
+end
+
+for order, mod in pairs(loaded_mods) do
+	local msg = 'loaded module ' .. tostring(order) .. '. "' .. mod .. '"'
+	vim.notify(msg, vim.log.levels.DEBUG)
 end
