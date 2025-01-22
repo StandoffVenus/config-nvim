8040092dac7a8e5c60cbef1a311695e871f22966
Liam Mueller <l.mueller@fetchrewards.com>
2025-01-22 13:02:27 -0600

    feat: minor updates; introduced table.to_pretty_string

--- lua/mule/plugins/lspconfig.lua
+++ lua/mule/plugins/lspconfig.lua
@@ -1,21 +1,64 @@
 -- Aliases for language to LSP
-local docker      = 'dockerls'
-local lua         = 'lua_ls'
-local protobuf    = 'buf_ls'
-local gleam       = 'gleam'
-local go          = 'gopls'
-local java        = 'jdtls'
-local terraform   = 'terraformls'
-local zig         = 'zls'
+local docker    = 'dockerls'
+local lua       = 'lua_ls'
+local gleam     = 'gleam'
+local go        = 'gopls'
+local java      = 'jdtls'
+local terraform = 'terraformls'
+local zig       = 'zls'
+local protobuf  = 'buf_ls'
+
+--- Serializes a value into a string, recursively iterating table fields.
+--- @param val any The value to serialize.
+--- @param name? string The name of a table field. Should not be set by callers; reserved for recursion.
+--- @param depth? number The depth of recursion. Should not be set by callers; reserved for recursion.
+--- @return string serialized The serialization of the value.
+local function table_tostring(val, name, depth)
+	depth = depth or 0
+	local tmp = string.rep(" ", depth)
+	if name then tmp = tmp .. name .. " = " end
+
+	local typeOfVal = type(val)
+	if typeOfVal == "table" then
+		tmp = tmp .. "{" .. "\n"
+		for k, v in pairs(val) do
+			tmp = tmp .. table_tostring(v, k, depth + 1) .. ",\n"
+		end
+
+		tmp = tmp .. string.rep(" ", depth) .. "}"
+	elseif typeOfVal == "number" or type(val) == "boolean" then
+		tmp = tmp .. tostring(val)
+	elseif typeOfVal == "string" then
+		tmp = tmp .. string.format("%q", val)
+	else
+		tmp = tmp .. "\"[inserializeable datatype:" .. typeOfVal .. "]\""
+	end
+
+	return tmp
+end
+
+--- Serializes a table into a more human-readable string.
+--- If the table is `nil`, an empty string is returned.
+--- @param tbl table The table to serialize.
+--- @return string serialized The serialization of the table.
+function table.to_pretty_string(tbl)
+	if tbl then
+		return table_tostring(tbl)
+	end
+
+	return ""
+end
 
 local lsp_servers = {
-	protobuf,
 	docker,
 	gleam,
 	go,
 	java,
 	terraform,
 	zig,
+	[protobuf] = {
+		cmd = { 'buf', 'beta', 'lsp', '--timeout=0', '--log-format=text', '--debug' },
+	},
 	[lua] = {
 		Lua = {
 			runtime = {
@@ -56,6 +99,7 @@ end
 
 local config      = function()
 	local lspconfig = require('lspconfig')
+	vim.lsp.set_log_level('debug')
 
 	require('mason').setup()
 	require('mason-lspconfig').setup()
@@ -68,7 +112,9 @@ local config      = function()
 		local settings
 		local lsp
 
-		-- If the key is a string, that means we have a configured server
+		-- As we iterate, we'll either have integers/numbers (indices)
+		-- or we'll have strings (keys). If the key is a string, that
+		-- means we have a configured server
 		if type(k) == 'string' then
 			lsp = k
 			settings = v
@@ -81,6 +127,13 @@ local config      = function()
 			capabilities = cmp_capabilities,
 			settings = settings,
 		}
+
+		-- FOR DEBUGGING
+		--local msg = "Loaded LSP configuration:" .. lsp
+		--if settings then
+		--	msg = msg .. "\n" .. table_tostring(settings)
+		--end
+		--vim.notify(msg)
 	end
 
 	vim.api.nvim_create_autocmd('BufWritePre', {
