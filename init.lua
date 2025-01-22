3766ef648f79daeed29933f3f1aaaf098f24a973
Liam Mueller <l.mueller@fetchrewards.com>
2025-01-22 14:23:38 -0600

    refactor: small changes to load script

--- init.lua
+++ init.lua
@@ -1,5 +1,31 @@
-require('mule.opts')
-require('mule.remaps')
-require('mule.lazy')
-require('mule.filetypes')
-require('mule.lsp_extensions')
+local DEBUG = vim.log.levels.DEBUG
+local ERROR = vim.log.levels.ERROR
+
+local modules = {
+	'opts',
+	'remaps',
+	'lazy',
+	'filetypes',
+	'lsp_extensions',
+	'path',
+}
+
+local config_dir = vim.fn.expand('~')
+if not config_dir then
+	local err --[[@as string]]
+	config_dir, err = vim.uv.os_homedir()
+	if not config_dir then
+		error(err)
+	end
+end
+
+local mod_base = 'mule'
+for _, mod_name in pairs(modules) do
+	local mod = mod_base .. '.' .. mod_name
+	local ok, err = pcall(require, mod)
+	if not ok then
+		vim.notify('failed to load module "' .. mod .. '": ' .. tostring(err), ERROR)
+	else
+		vim.notify('loaded module "' .. mod .. '"', DEBUG)
+	end
+end
